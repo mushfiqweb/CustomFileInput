4060f53b7532195051f6fe122b2d6451030cc117
Mushfiqur Rahman Shishir <hello@mushfiqweb.com>
2020-08-27 06:35:22 +0600

    fix

--- custom-file-input.js
+++ custom-file-input.js
@@ -4,30 +4,27 @@
 
 'use strict';
 
-;( function ( document, window, index )
-{
-	var inputs = document.querySelectorAll( '.inputfile' );
-	Array.prototype.forEach.call( inputs, function( input )
-	{
-		var label	 = input.nextElementSibling,
+(function (document, window, index) {
+	var inputs = document.querySelectorAll('.inputfile');
+	Array.prototype.forEach.call(inputs, (input) => {
+		var label = input.nextElementSibling,
 			labelVal = label.innerHTML;
 
-		input.addEventListener( 'change', function( e )
-		{
+		input.addEventListener('change', function (e) {
 			var fileName = '';
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+			if (this.files && this.files.length > 1)
+				fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
 			else
-				fileName = e.target.value.split( '\\' ).pop();
+				fileName = e.target.value.split('\\').pop();
 
-			if( fileName )
-				label.querySelector( 'span' ).innerHTML = fileName;
+			if (fileName)
+				label.querySelector('span').innerHTML = fileName;
 			else
 				label.innerHTML = labelVal;
 		});
 
 		// Firefox bug fix
-		input.addEventListener( 'focus', function(){ input.classList.add( 'has-focus' ); });
-		input.addEventListener( 'blur', function(){ input.classList.remove( 'has-focus' ); });
+		input.addEventListener('focus', function () { input.classList.add('has-focus'); });
+		input.addEventListener('blur', function () { input.classList.remove('has-focus'); });
 	});
-}( document, window, 0 ));
+}(document, window, 0));
